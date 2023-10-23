e6c90164ad5924d3b64a14e6597910d84f50dada
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-10-23 18:36:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        int[] array2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        bool areIdentical = AreArraysIdentical(array1, array2);
+
+        if (areIdentical)
+        {
+            Console.WriteLine("Arrays are identical.");
+        }
+        else
+        {
+            Console.WriteLine("Arrays are not identical.");
+        }
+    }
+
+    static bool AreArraysIdentical(int[] arr1, int[] arr2)
+    {
+        if (arr1.Length != arr2.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
